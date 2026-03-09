8554a051b2cc578f73c9a42870484f3b15f440ed
g-akca <gok.akca66@gmail.com>
2026-03-10 01:58:26 +0300

    Add dark overlay when menu is opened

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const openMenuBtn = document.getElementById("open-menu");
 const closeMenuBtn = document.getElementById("close-menu");
 const navBar = document.getElementById("main-nav");
+const overlay = document.getElementById("overlay");
 
 function handleResize() {
-    if (window.innerWidth >= 960) {
+    if (window.innerWidth >= 900) {
         navBar.classList.remove("menu");
         openMenuBtn.setAttribute("aria-expanded", "false");
+        overlay.classList.remove("active");
     }
 }
 
@@ -14,9 +16,18 @@ window.addEventListener("resize", handleResize);
 openMenuBtn.addEventListener("click", () => {
     navBar.classList.add("menu");
     openMenuBtn.setAttribute("aria-expanded", "true");
+    overlay.style.display = "block";
+    setTimeout(() => overlay.classList.add("active"), 1);
 });
 
 closeMenuBtn.addEventListener("click", () => {
     navBar.classList.remove("menu");
     openMenuBtn.setAttribute("aria-expanded", "false");
+    overlay.classList.remove("active");
+});
+
+overlay.addEventListener("click", () => {
+    navBar.classList.remove("menu");
+    openMenuBtn.setAttribute("aria-expanded", "false");
+    overlay.classList.remove("active");
 });
